c11cb1893d24bc2d8a6753a5545b88488fdac21c
Adrian Hedqvist <adrian@tollyx.net>
2018-05-29 15:59:06 +0200

    Add Pythagoras

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -11,5 +11,9 @@ namespace GitDemo
         static void Main(string[] args)
         {
         }
+
+        static double Pythagoras(double a, double b) {
+            return Math.Sqrt(a * a + b * b);
+        }
     }
 }
